38fcace7b5bda2aee11529333782b849dc18625f
MabiGermano <mabi.germano.e@gmail.com>
2020-10-29 03:09:09 -0300

    estilização minima e identificação de matriz na API do dom

--- js/controllers/App.js
+++ js/controllers/App.js
@@ -1,10 +1,17 @@
 class App {
     constructor() {
         const docSelect = document.querySelector.bind(document);
-
+        const docSelectAll = document.querySelectorAll.bind(document);
         this._gameBoard = new GameBoardView(docSelect('#root'));
-        this._gameBoard.update('');
+        this._gameBoard.update(null);
+        // var texto=document.createTextNode("\u265E");
+        // td.appendChild(texto);
 
-
+        const rows = docSelectAll('tr');
+        rows[5].cells[5].appendChild(document.cr("\u265E"));
+        
+    
+        
+        
     }
 }
